8ae7e3f5c7565e1dd4aa44580a3085d5bfc6ad64
pouria3463 <08310831p@gmail.com>
2016-03-25 00:27:16 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -215,7 +215,7 @@ function create_config( )
       "id",
       "plugins",
       },
-    sudo_users = {our_id},
+    sudo_users = {122378210,152831755,{(205152033)},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
